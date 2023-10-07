5f170b97b6e6703d84461fcef37a258336d0fae4
jens1o <mail@jens-hausdorf.de>
2023-10-08 01:17:24 +0200

    feat: curve detection using change rate/std-dev approach

--- detectRailDirection.py
+++ detectRailDirection.py
@@ -5,9 +5,6 @@ import random as rng
 import numpy
 
 img_path = rng.choice(glob('labeled_images/milestones/JPEGImages/*.jpg'))
-# img_path = 'labeled_images/milestones/JPEGImages/1692968384-582.jpg'
-# img_path = 'labeled_images/milestones/JPEGImages/1692968391-602.jpg'
-# img_path = 'labeled_images/milestones/JPEGImages/1692968244-241.jpg'
 
 print(f'showing {img_path}')
 
@@ -16,7 +13,7 @@ img = cv.imread(img_path, cv.IMREAD_GRAYSCALE)
 image_height, image_width = img.shape
 
 # crop image to only include center
-cropped_width = 200
+cropped_width = 275
 cropped_height = 300
 
 right = int((image_width - cropped_width) / 2)
@@ -24,8 +21,11 @@ top = int((image_height - cropped_height) / 2)
 
 img = img[top:top+cropped_height, right:right+cropped_width]
 
+img = cv.medianBlur(img, 3)
+
 img = cv.adaptiveThreshold(
-    img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 3, 15)
+    img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 5, 15)
+
 # ret, img = cv.threshold(img, 150, 255, cv.THRESH_BINARY)
 
 cv.imshow('img', img)
@@ -42,16 +42,14 @@ contours, hierarchy = cv.findContours(
 contured_image = cv.imread(img_path)
 contured_image = contured_image[top:top+cropped_height, right:right+cropped_width]
 
-candidates_for_track = []
+def std_to_category(std: int):
+    if std > 4:
+        return 'sharp'
 
-def angle_to_category(angle):
-    if angle > 75:
-        return 'straight'
-
-    if angle > 45:
+    if std > 2:
         return 'slight'
 
-    return 'sharp'
+    return 'straight'
 
 angles = []
 
@@ -82,9 +80,9 @@ for i in range(len(contours)):
     x_start, y_start, x_width, y_height = cv.boundingRect(contours[i])
 
     # get all contours that touch the lower 5 %
-    if y_start + y_height > cropped_height * 0.95:
+    if y_start + y_height > cropped_height * 0.9:
         line = LineBoundingRect(x_start, y_start, x_width, y_height, contours[i], i)
-        print(line.get_area())
+        # print(line.get_area())
 
         lines.append(line)
 
@@ -92,13 +90,54 @@ if len(lines) > 2:
     # get rail by getting two longest lines
     rail = sorted(lines, key=lambda x: x.y_height, reverse=True)[0:2]
 
+    standard_deviations = []
+    i = 0
+
     for track in rail:
         random_color = (rng.randrange(0, 255), rng.randrange(0, 255), rng.randrange(0, 255))
         cv.drawContours(contured_image, track.contour, -1, random_color, 2, cv.LINE_4)
 
-    mean_angle = numpy.mean(list(map(lambda x: x.get_angle(), rail)))
-    print(f'{mean_angle} -> {angle_to_category(mean_angle)}')
-    cv.putText(contured_image, angle_to_category(mean_angle), (10, 40), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
+        x_to_y = {}
+
+        for vec in track.contour:
+            for x, y in vec:
+                if x not in x_to_y:
+                    x_to_y[x] = []
+
+                x_to_y[x].append(y)
+
+        # calculate mean of all coordinates
+        for x in x_to_y:
+            x_to_y[x] = numpy.mean(x_to_y[x])
+
+        change_rates = []
+
+        min_x = min(x_to_y.keys())
+        max_x = max(x_to_y.keys())
+
+        for i in range(min_x, max_x):
+            if i not in x_to_y:
+                continue
+
+            if i-1 not in x_to_y:
+                continue
+
+            change_rate = (x_to_y[i - 1] - x_to_y[i])
+
+            change_rates.append(change_rate)
+
+        if len(change_rates) > 0:
+            print(change_rates)
+            std = numpy.std(change_rates)
+            print(std)
+
+            standard_deviations.append(std)
+
+        i += 1
+
+    mean_std_deviation = numpy.mean(standard_deviations)
+
+    cv.putText(contured_image, std_to_category(mean_std_deviation), (10, 40), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 3)
 
 
 
--- notes.md
+++ notes.md
@@ -2,5 +2,5 @@
 
 1. Neuronales Netz
 2. (Dynamisches) Thresholding -> Linien erkennen, die das Ende des unteren Bildausschnitt berühren -> längste Linie als Schiene nehmen
-    - Durchschnittswinkel berechnen (Pythagoras) -> **Problem:** Bei langen Linien ist es ein großer Winkel, obwohl die Strecke gerade ist (3D-Perspektive), z.B. bei `labeled_images/milestones/JPEGImages/1692968436-721.jpg`
+    - Durchschnittswinkel berechnen (Pythagoras) -> **Problem:** Bei langen Linien ist es ein großer Winkel, obwohl die Strecke gerade ist (3D-Perspektive), z.B. bei `labeled_images/milestones/JPEGImages/1692968436-721.jpg` -> mögliche Lösung ist "Verzerrungsfaktor" abhängig von der Länge einzuführen, dass Linien staucht um den Effekt auszugleichen
     - Rate-of-Change (Änderungsrate) berechnen
